f7927f3972e63161f8bce3887cd6e239ec93e639
Constantine_Mukhin <mol.Kostya@mail.ru>
2023-10-20 23:28:54 +0500

    Added: * Handler and HandlerFunc * Readme for Postman * Data for postman

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+type handler struct {
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	instance := &handler{}
+	http.Handle("/hello", instance)
+	http.HandleFunc("/hello_func", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from func, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
